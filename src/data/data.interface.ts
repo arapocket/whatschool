556f3ff2c9f6aae04b02f2a8ab20dbd67fbb3fae
arapocket <arakazaryan@me.com>
2017-06-13 17:09:25 -0700

    added data

---
+++ src/data/data.interface.ts
@@ -0,0 +1,42 @@
+export interface Data {
+    school: string;
+    total: number;
+    facebook: number;
+    google: number;
+    apple: number;
+    amazon: number;
+    microsoft: number;
+    oracle: number;
+    ibm: number;
+    cisco: number;
+    intel: number;
+    hp: number;
+    boeing: number;
+    jpl: number;
+    northrop: number;
+    qualcomm: number;
+    lockheed: number;
+    raytheon: number;
+    kaiser: number;
+    sce: number;
+    bloomberg: number;
+    goldman: number;
+    att: number;
+    tesla: number;
+    rollsroyce: number;
+    ford: number;
+    gm: number;
+    ebay: number;
+    salesforce: number;
+    linkedin: number;
+    vmware: number;
+    nvidia: number;
+    uber: number;
+    slack: number;
+    reddit: number;
+    geaviation: number;
+    delphi: number;
+    workday: number;
+    expedia: number;
+
+}
--- src/pages/schools/schools.ts
+++ src/pages/schools/schools.ts
@@ -1,24 +1,20 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component, OnInit } from '@angular/core';
+import {Data} from '../../data/data.interface'
+import datas from '../../data/data'
 
-/**
- * Generated class for the SchoolsPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
-@IonicPage()
+
+
 @Component({
   selector: 'page-schools',
   templateUrl: 'schools.html',
 })
-export class SchoolsPage {
+export class SchoolsPage implements OnInit {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+dataCollection: {datas: Data[]}[];
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad SchoolsPage');
-  }
+ngOnInit() {
+  this.dataCollection = datas;
 
 }
+}
+
